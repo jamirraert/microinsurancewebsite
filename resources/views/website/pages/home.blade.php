a447098004135d385e0aa98014e4b5b173494b1c
unknown <chan>
2024-01-08 15:54:05 +0800

    Enhancement

--- resources/views/website/pages/home.blade.php
+++ resources/views/website/pages/home.blade.php
@@ -310,19 +310,22 @@
                 <table>
                     <tr>
                         <td class="td-label">Email:</td>
-                        <td class="td-content">support@ruralnet.ph</td>
+                        <td class="td-content" style="color: rgb(255,255,255)">support@ruralnet.ph</td>
                     </tr>
+
                     <tr>
                         <td class="td-label">Toll-Free:</td>
-                        <td class="td-content"> 1800 1320 0211</td>
+                        <td class="td-content" style="color: rgb(255,255,255)">&nbsp;1800 1320 0211</td>
                     </tr>
+
                     <tr>
                         <td class="td-label">Globe:</td>
-                        <td class="td-content"> (028) 876 1428</td>
+                        <td class="td-content" style="color: rgb(255,255,255)">&nbsp;(028) 876 1428</td>
                     </tr>
+
                     <tr>
                         <td class="td-label">PLDT:</td>
-                        <td class="td-content"> (032) 342 7826</td>
+                        <td class="td-content" style="color: rgb(255,255,255)">&nbsp;(032) 342 7826</td>
                     </tr>
                     <tr>
                         <td colspan="2">&nbsp;</td>
